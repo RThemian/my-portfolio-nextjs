1002b66b19be05eaa8abdbf8f00ddeb8664ea4da
Daniel JS <daniel@mydeveloperjourney.com>
2023-05-03 19:54:01 -0500

    wip: adds layout and init styles

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                Copyright &copy; All Rights Reserved {new Date().getFullYear()} Daniel Scott
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+function Header() {
+    return (
+        <header>
+            {/* TODO: Add image tag here */}
+            <h1>Hi, I'm Daniel Scott</h1>
+        </header>
+    );
+}
+
+export default Header;
