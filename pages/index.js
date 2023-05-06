498f7e8cc2a2b0f45bd350a54573264eee7d4a0f
Daniel JS <daniel@mydeveloperjourney.com>
2023-05-06 15:24:46 -0500

    wip: adds most recent changes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import styles from '@/styles/Home.module.css';
 import ProjectCard from '@/components/ProjectCard';
 import projectData from '@/public/project-data';
 
-console.log(projectData)
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,10 +24,24 @@ export default function Home() {
             Libero, unde? Odit, maiores aliquid est, saepe fugit veniam 
             pariatur magni quasi itaque dolorum sunt repudiandae adipisci, 
             iste expedita voluptatem corporis unde.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Libero, unde? Odit, maiores aliquid est, saepe fugit veniam 
+            pariatur magni quasi itaque dolorum sunt repudiandae adipisci, 
+            iste expedita voluptatem corporis unde.
           </p>
         </section>
-        <section className={styles.pageSection}>
+        <section className={`${styles.pageSection} ${styles.skillsSection}`}>
           <h2>Skills</h2>
+          <div>
+            <Image src="/html5_icon.png" width={64} height={64} alt="html5"/>
+            <Image src="/css3_icon.png" width={64} height={64} alt="css3"/>
+            <Image src="/javascript_icon.png" width={64} height={64} alt="javascript"/>
+            <Image src="/react_icon.png" width={64} height={64} alt="react"/>
+            <Image src="/sass_icon.png" width={64} height={64} alt="sass"/>
+            <Image src="/nodejs_icon.png" width={64} height={64} alt="nodejs"/>
+            <Image src="/server_icon.png" width={64} height={64} alt="mongodb"/>
+            <Image src="/python_icon.png" width={64} height={64} alt="python"/>
+          </div>
         </section>
         <section className={styles.pageSection}>
           <h2>Projects</h2>
@@ -48,6 +61,7 @@ export default function Home() {
         </section>
         <section className={styles.pageSection}>
           <h2>Contact</h2>
+
         </section>
       </main>
     </>
