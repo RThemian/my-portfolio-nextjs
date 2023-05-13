bf0d95d39131790e8ac16ea2b30f0a0d081bcff2
Daniel JS <daniel@mydeveloperjourney.com>
2023-05-13 09:51:19 -0500

    wip: installs and configs font-awesome

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 function Footer() {
     return (
-        <footer>
+        <footer className="footer">
             <p>
                 Copyright &copy; All Rights Reserved {new Date().getFullYear()} Daniel Scott
             </p>
+            <section className="social-icons">
+                <a href="https://github.com/myDeveloperJourney" target="_blank">
+                    <FontAwesomeIcon icon={faGithubSquare} className="icon" />
+                </a>
+                <a href="https://www.linkedin.com/in/daniel-j-scott/" target="_blank">
+                    <FontAwesomeIcon icon={faLinkedin} className="icon" />
+                </a>
+            </section>
         </footer>
     );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css';
 import Layout from '@/components/Layout';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 
 export default function App({ Component, pageProps }) {
   return (
