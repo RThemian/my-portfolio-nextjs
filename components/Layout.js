4478636b8268dbbdce34817d0090d573451af9aa
Daniel JS <daniel@mydeveloperjourney.com>
2023-05-13 10:10:25 -0500

    wip: adds mobile styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,18 @@
 import Nav from './Nav';
 import Header from './Header';
 import Footer from './Footer';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 function Layout(props) {
     return (
-        <>
+        <div className={inter.className}>
             <Nav />
             <Header />
             {props.children}
             <Footer />
-        </>
+        </div>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import ProjectCard from '@/components/ProjectCard';
 import Form from '@/components/Form';
 import projectData from '@/public/project-data';
 
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -18,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${inter.className} ${styles.homeMain}`}>
+      <main className={styles.homeMain}>
         <section className={styles.pageSection}>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
